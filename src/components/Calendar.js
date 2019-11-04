a2f2780e37a12cbc92f41cb815a4e8d70433b557
eva-lm <eva.lm1810@gmail.com>
2019-11-04 19:27:02 +0100

    save data

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,7 +1,10 @@
 import React from "react";
+//import Editor from "./Editor";
 
-const Calendar = () => {
-  return <p>Soy el calendario</p>;
+const Calendar = props => {
+  console.log("soy las props del calendario", props);
+  const { editor } = props;
+  return <p>{editor.face}</p>;
 };
 
 export default Calendar;
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,34 +1,140 @@
 import React from "react";
+import Calendar from "./Calendar";
 import "../stylesheets/Editor.scss";
 
-const Editor = () => {
-  return (
-    <form className="form">
-      <label htmlFor="date">
-        Fecha
-        <input id="date" type="date" placeholder="fecha de hoy" />
-      </label>
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <label htmlFor="state">
-        Estado
-        <legend>
-          <input id="state" type="checkbox" value=":)" /> :)
-          <input id="state" type="checkbox" value=":(" /> :(
-        </legend>
-      </label>
+    this.state = {
+      editor: {
+        date: "",
+        face: "",
+        message: ""
+      }
+    };
+    this.handleDate = this.handleDate.bind(this);
+    this.handleFace = this.handleFace.bind(this);
+    this.handleMessage = this.handleMessage.bind(this);
+    this.saveData = this.saveData.bind(this);
+  }
+  handleDate(ev) {
+    const date = ev.target.value;
+    this.setState(prevState => {
+      return {
+        editor: {
+          ...prevState.editor,
+          ...this.state.date,
+          date
+        }
+      };
+    });
+  }
 
-      <label htmlFor="message">
-        Mensaje
-        <input
-          id="message"
-          type="text"
-          placeholder="¿Por qué es un buen día?"
-        />
-      </label>
-      <button className="save">Guardar</button>
-      <button className="cancel">Cancelar</button>
-    </form>
-  );
-};
+  handleFace(ev) {
+    const face = ev.target.value;
+    const checked = ev.target.checked;
+    if (checked === true) {
+      return this.setState(prevState => {
+        return {
+          editor: {
+            ...prevState.editor,
+            ...this.state.face,
+            face
+          }
+        };
+      });
+    } else {
+      return checked === false;
+    }
+  }
+
+  handleMessage(ev) {
+    const message = ev.target.value;
+    this.setState(prevState => {
+      return {
+        editor: {
+          ...prevState.editor,
+          ...this.state.message,
+          message
+        }
+      };
+    });
+  }
+
+  saveData(ev) {
+    ev.preventDefault();
+    this.setState(prevState => {
+      return {
+        editor: {
+          ...prevState.editor
+        }
+      };
+    });
+    alert(this.state);
+  }
+
+  render() {
+    const { editor, date, face, message } = this.state;
+
+    return (
+      <form className="form">
+        <fieldset>
+          <label htmlFor="date">Fecha</label>
+          <input
+            id="date"
+            type="date"
+            className="form__date"
+            onChange={this.handleDate}
+            value={date}
+            placeholder="fecha de hoy"
+          />
+        </fieldset>
+
+        <fieldset>
+          <legend>Estado</legend>
+          <label htmlFor="happy"> =D </label>
+          <input
+            id="happy"
+            className="face_happy"
+            onChange={this.handleFace}
+            value=":)"
+            type="checkbox"
+            checked={face}
+          />
+          <label htmlFor="sad"> :/ </label>
+          <input
+            id="sad"
+            className="face_sad"
+            onChange={this.handleFace}
+            type="checkbox"
+            value=":("
+            checked={face}
+          />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor="message">
+            Mensaje
+            <input
+              id="message"
+              type="text"
+              onChange={this.handleMessage}
+              value={message}
+              placeholder="¿Por qué es un buen día?"
+            />
+          </label>
+        </fieldset>
+        <button className="save" onClick={this.saveData}>
+          Guardar
+        </button>
+        <button className="cancel">Cancelar</button>
+        <span>{JSON.stringify(this.state)}</span>
+
+        <Calendar editor={editor} />
+      </form>
+    );
+  }
+}
 
 export default Editor;
